e337e987fceb35b7e47237746da9b442f712641f
93294 <9329496@mail.ru>
2022-11-20 12:35:44 +0300

    Initial commit

---
+++ src/main/java/com/calculator/lessontesting/LessonTestingApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.lessontesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonTestingApplication.class, args);
+    }
+
+}
